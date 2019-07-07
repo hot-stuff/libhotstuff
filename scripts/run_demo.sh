a442616edf97dc65f6e768d249eb318fa317c747
Determinant <ted.sybil@gmail.com>
2019-07-06 23:01:10 -0400

    move the demo app to examples/

--- scripts/run_demo.sh
+++ scripts/run_demo.sh
@@ -5,8 +5,8 @@ if [[ $# -gt 0 ]]; then
 fi
 for i in "${rep[@]}"; do
     echo "starting replica $i"
-    #valgrind --leak-check=full ./hotstuff-app --conf hotstuff-sec${i}.conf > log${i} 2>&1 &
-    #gdb -ex r -ex bt -ex q --args ./hotstuff-app --conf hotstuff-sec${i}.conf > log${i} 2>&1 &
-    ./hotstuff-app --conf hotstuff-sec${i}.conf > log${i} 2>&1 &
+    #valgrind --leak-check=full ./examples/hotstuff-app --conf hotstuff-sec${i}.conf > log${i} 2>&1 &
+    #gdb -ex r -ex bt -ex q --args ./examples/hotstuff-app --conf hotstuff-sec${i}.conf > log${i} 2>&1 &
+    ./examples/hotstuff-app --conf ./hotstuff-sec${i}.conf > log${i} 2>&1 &
 done
 wait
